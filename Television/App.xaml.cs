fda0efe4c717bcd084f819780d02d5bd33e9b697
Koen D'Heedene <koene.dheedene@gmail.com>
2021-11-25 15:04:11 +0100

    moved methods from app.xaml.cs to mainwindow.xaml.cs

--- Television/App.xaml.cs
+++ Television/App.xaml.cs
@@ -13,9 +13,7 @@ namespace Television
     /// </summary>
     public partial class App : Application
     {
-        SqlRepository SqlRep = new SqlRepository();
-        private static Timer buttonTimer;
-        private static bool timeOut = false;
+  
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             // Create the startup window
@@ -26,150 +24,9 @@ namespace Television
             wnd.Show();
 
             //	SqlRep.CheckDatabaseOnchange();
-            onDbChanged();
-            SetTimer();
+         
         }
-        public void onDbChanged()
-        {
-
-            string connectionString = Defaults.DbConnString;
-            var changeListener = new DatabaseChangeListener(connectionString);
-
-            changeListener.OnChange += () =>
-            {
-                changeListener.Start(@"SELECT [button] FROM [dbo].[Commands]");
-                DbChanged();
-            };
-            changeListener.Start(@"SELECT [button] FROM [dbo].[Commands]");
-
-        }
-        private void DbChanged()
-        {
-            List<Command> commands = new List<Command>();
-            commands = SqlRep.GetFirstThree();
-            InterpreteDbCommands(commands);
-        }
-        private int InterpreteDbCommands(List<Command> Commands)
-        {
-            int sendCommand = 0;
-            List<int> intCommands = TranslateCommand(Commands);
-
-            if (intCommands[0] < 10)
-            {
-                sendCommand = NumberInput(intCommands, Commands);
-            }
-
-            else if (intCommands[0] == 255)
-            {
-                Debug.WriteLine("ongeldige invoer");
-            }
-            else
-            {
-                sendCommand = intCommands[0] + 1000;
-            }
-            Debug.WriteLine(sendCommand);
-
-            return sendCommand;
-        }
-        private int TranslateCommand(Command Command)
-        {
-
-            int Result;
-            bool succes = int.TryParse(Command.command, out Result);
-            if (succes)
-            {
-                return Result;
-            }
-            return 255;
-
-        }
-        private List<int> TranslateCommand(List<Command> Commands)
-        {
-            List<int> NewList = new List<int>();
-            for (int i = 0; i < Commands.Count; i++)
-            {
-                NewList.Add(TranslateCommand(Commands[i]));
-            }
-            return NewList;
-        }
-        private int NumberInput(List<int> intcommands, List<Command> commands)
-        {
-            int value = -1;
-            buttonTimer.Enabled = true;
-
-            switch (intcommands.Count)
-            {
-                case 1:
-                    Debug.WriteLine(DateTime.Now.Subtract(commands[0].DT).TotalSeconds);
-                    if (DateTime.Now.Subtract(commands[0].DT).TotalSeconds > 3 || timeOut)
-                    {
-                        Debug.WriteLine("1 waarde");
-                        value = intcommands[0];
-                    }
-                    break;
-                case 2:
-                    Debug.WriteLine(DateTime.Now.Subtract(commands[0].DT).TotalSeconds);
-                    if (intcommands[1] < 10)
-                    {
-                        if (DateTime.Now.Subtract(commands[0].DT).TotalSeconds > 3 || timeOut)
-                        {
-                            Debug.WriteLine("2 waarde");
-                            value = intcommands[0] * 10 + intcommands[1];
-                        }
-                    }
-                    if (intcommands[1] > 10)
-                    {
-                        Debug.WriteLine("2 waarde");
-                        value = intcommands[0];
-                        buttonTimer.Enabled = false;
-                    }
-                    break;
-                case 3:
-                    Debug.WriteLine(DateTime.Now.Subtract(commands[0].DT).TotalSeconds);
-                    if (intcommands[1] < 10 && intcommands[2] < 10)
-                    {
-                        Debug.WriteLine("3 waarde");
-                        value = intcommands[0] * 100 + intcommands[1] * 10 + intcommands[2];
-                    }
-                    else if (intcommands[1] < 10 && intcommands[2] >= 10)
-                    {
-                        value = intcommands[0] * 10 + intcommands[1];
-                    }
-                    else if (intcommands[1] >= 10)
-                    {
-                        value = intcommands[0];
-
-                    }
-                    buttonTimer.Enabled = false;
-                    break;
-
-                default:
-                    break;
-            }
-            if (value >= 0)
-            {
-                buttonTimer.Enabled = false;
-            }
-
-            return value;
-        }
-        private void SetTimer()
-        {
-            Debug.WriteLine("starttimer");
-            // Create a timer with a three second interval.
-            buttonTimer = new System.Timers.Timer(3000);
-            // Hook up the Elapsed event for the timer. 
-            buttonTimer.Elapsed += OnTimedButtonEvent;
-            buttonTimer.AutoReset = false;
-            //buttonTimer.Enabled = true;
-        }
-        private void OnTimedButtonEvent(Object source, ElapsedEventArgs e)
-        {
-            timeOut = true;
-            DbChanged();
-            buttonTimer.Enabled = false;
-            timeOut = false;
-        }
+        
 
     }
 }
--- Television/MainWindow.xaml.cs
+++ Television/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Data.SqlClient;
+using System.Timers;
+using System.Threading;
+using System.Windows.Threading;
+using Timer = System.Timers.Timer;
 
 namespace Television
 {
@@ -22,13 +26,39 @@ namespace Television
     /// </summary>
     public partial class MainWindow : Window
     {
+        RecvBroadcst Receiver = new RecvBroadcst();
         TV tv = new TV();
+        SqlRepository SqlRep = new SqlRepository();
+        private static Timer buttonTimer;
+        private static bool timeOut = false;
+        private static List<int> longchannel = new List<int>();
+        
+
         public MainWindow()
         {
             InitializeComponent();
-            
-     
-           
+
+            onDbChanged();
+            SetTimer();
+            Thread t = new Thread(CheckData);
+            t.Start();
+        }
+
+        public void CheckData()
+        {
+            while (true)
+            {
+                if (RecvBroadcst.receivedinputcommands.Count() != 0)
+                {
+                    byte[] received = RecvBroadcst.receivedinputcommands.First();
+                    RecvBroadcst.receivedinputcommands.Remove(received);                    
+                    if (received[0] == 1 ) // 1 = from remote
+                    {
+                        debuginfo($"from socket:{received[1]}");
+                        InterpreteDbCommand(received[1].ToString());
+                    }
+                }
+            }
         }
         private void OnOff_Click(object sender, RoutedEventArgs e)
         {
@@ -85,5 +115,203 @@ namespace Television
             tv.SourceDown();
             DispSrc.Content = (Defaults.Sources)tv.Source;
         }
+
+        public void onDbChanged()
+        {
+
+            string connectionString = Defaults.DbConnString;
+            var changeListener = new DatabaseChangeListener(connectionString);
+
+            changeListener.OnChange += () =>
+            {
+                changeListener.Start(@"SELECT [button] FROM [dbo].[Commands]");
+                DbChanged();
+            };
+            changeListener.Start(@"SELECT [button] FROM [dbo].[Commands]");
+
+        }
+        private void DbChanged()
+        {
+            List<Command> commands = new List<Command>();
+            commands = SqlRep.GetFirstThree();
+            Command command = SqlRep.GetFirst();
+            if(command.command != null) InterpreteDbCommand(command.command);
+        }
+
+        private void InterpreteDbCommand(string data)
+        {            
+            SqlRep.DeleteFirst();
+            bool result = int.TryParse(data, out int tag);
+
+            if (tag < 10)
+            {
+                if (buttonTimer.Enabled) buttonTimer.Enabled = false;                
+                longchannel.Add(tag);
+                if (longchannel.Count == 3)
+                {                    
+                    makeChannelSetCommand();
+                    return;
+                }
+                buttonTimer.Enabled = true;
+            }
+            if (tag == 10)
+            {
+                OnOff_Click(this, null); // still gives an error
+            }
+
+        }
+        private void OnTimedButtonEvent(Object source, ElapsedEventArgs e)
+        {
+            makeChannelSetCommand();     
+            buttonTimer.Enabled = false;
+        }
+        private void makeChannelSetCommand()
+        {
+            int channel = 0;
+            switch (longchannel.Count)
+            {
+                case 1:
+                    channel = longchannel[0];
+                    break;
+                case 2:
+                    channel = longchannel[0] * 10 + longchannel[1];
+                    break;
+                case 3:
+                    channel = longchannel[0]*100+ longchannel[1] * 10 + longchannel[2];
+                    break;
+                default:
+                    break;
+            }
+            tv.SetChannel(channel);
+            this.Dispatcher.Invoke(() =>
+            {
+                DispCh.Content = (Defaults.Channels)tv.Channel;
+            });
+            longchannel.Clear();
+        }
+    
+        public void debuginfo (string text)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                tbo_info.AppendText(text + "\r\n");
+            });
+        }
+
+        private int InterpreteDbCommands(List<Command> Commands)
+        {
+            int sendCommand = 0;
+            List<int> intCommands = TranslateCommand(Commands);
+
+            if (intCommands[0] < 10)
+            {
+                sendCommand = NumberInput(intCommands, Commands);
+            }
+
+            else if (intCommands[0] == 255)
+            {
+                Debug.WriteLine("ongeldige invoer");
+            }
+            else
+            {
+                sendCommand = intCommands[0] + 1000;
+            }
+            Debug.WriteLine(sendCommand);
+
+            return sendCommand;
+        }
+        private int TranslateCommand(Command Command)
+        {
+
+            int Result;
+            bool succes = int.TryParse(Command.command, out Result);
+            if (succes)
+            {
+                return Result;
+            }
+            return 255;
+
+        }
+        private List<int> TranslateCommand(List<Command> Commands)
+        {
+            List<int> NewList = new List<int>();
+            for (int i = 0; i < Commands.Count; i++)
+            {
+                NewList.Add(TranslateCommand(Commands[i]));
+            }
+            return NewList;
+        }
+        private int NumberInput(List<int> intcommands, List<Command> commands)
+        {
+            int value = -1;
+            buttonTimer.Enabled = true;
+
+            switch (intcommands.Count)
+            {
+                case 1:
+                    Debug.WriteLine(DateTime.Now.Subtract(commands[0].DT).TotalSeconds);
+                    if (DateTime.Now.Subtract(commands[0].DT).TotalSeconds > 3 || timeOut)
+                    {
+                        Debug.WriteLine("1 waarde");
+                        value = intcommands[0];
+                    }
+                    break;
+                case 2:
+                    Debug.WriteLine(DateTime.Now.Subtract(commands[0].DT).TotalSeconds);
+                    if (intcommands[1] < 10)
+                    {
+                        if (DateTime.Now.Subtract(commands[0].DT).TotalSeconds > 3 || timeOut)
+                        {
+                            Debug.WriteLine("2 waarde");
+                            value = intcommands[0] * 10 + intcommands[1];
+                        }
+                    }
+                    if (intcommands[1] > 10)
+                    {
+                        Debug.WriteLine("2 waarde");
+                        value = intcommands[0];
+                        buttonTimer.Enabled = false;
+                    }
+                    break;
+                case 3:
+                    Debug.WriteLine(DateTime.Now.Subtract(commands[0].DT).TotalSeconds);
+                    if (intcommands[1] < 10 && intcommands[2] < 10)
+                    {
+                        Debug.WriteLine("3 waarde");
+                        value = intcommands[0] * 100 + intcommands[1] * 10 + intcommands[2];
+                    }
+                    else if (intcommands[1] < 10 && intcommands[2] >= 10)
+                    {
+                        value = intcommands[0] * 10 + intcommands[1];
+                    }
+                    else if (intcommands[1] >= 10)
+                    {
+                        value = intcommands[0];
+
+                    }
+                    buttonTimer.Enabled = false;
+                    break;
+
+                default:
+                    break;
+            }
+            if (value >= 0)
+            {
+                buttonTimer.Enabled = false;
+            }
+
+            return value;
+        }
+        private void SetTimer()
+        {
+            Debug.WriteLine("starttimer");
+            // Create a timer with a three second interval.
+            buttonTimer = new System.Timers.Timer(3000);
+            // Hook up the Elapsed event for the timer. 
+            buttonTimer.Elapsed += OnTimedButtonEvent;
+            buttonTimer.AutoReset = false;
+            //buttonTimer.Enabled = true;
+        }
+        
     }
 }
